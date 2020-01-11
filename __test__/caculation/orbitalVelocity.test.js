cd8a14b0487e3deac56c55e2b78312347013095f
Luke Ruokaismak <luke@rmaki.com.au>
2020-01-11 18:37:35 +1100

    refactor orbital velocity

--- __test__/caculation/orbitalVelocity.test.js
+++ __test__/caculation/orbitalVelocity.test.js
@@ -2,5 +2,7 @@
 const orbitalVelocity = require("../../lib/caculations/orbitalVelocity");
 
 test("caculates orbital speed around Kerbin at 100km", () => {
-  expect(orbitalVelocity(100000)).toMatch("2246.14 m/s");
+  expect(orbitalVelocity(100000, 5.2915158 * 10 ** 22, 600000)).toMatch(
+    "2246.14 m/s"
+  );
 });
