6cf7b85c6e4fd021f0c83e0f3d0fad153d1bce28
Luke Ruokaismak <luke@rmaki.com.au>
2020-01-15 20:32:05 +1100

    add rocket equation

--- bin/index.js
+++ bin/index.js
@@ -1,8 +1,35 @@
 #!/usr/bin/env node
-const cliStart = require("../lib/program");
+const program = require("commander");
+const inquirer = require("inquirer");
+
+const sortAnswers = require("../lib/sortAnswers");
 
 const main = () => {
-  cliStart();
+  program.version("0.0.1");
+
+  program
+    .command("calc")
+    .alias("c")
+    .description("Presents choice of CLI calculations")
+    .action(() => {
+      inquirer
+        .prompt([
+          {
+            type: "list",
+            name: "calculations",
+            message: "What do you want to caculate?",
+            choices: [
+              "Rocket Equation",
+              "Vis Viva Equation",
+              "Velocity at a specific orbit"
+            ]
+          }
+        ])
+        .then(answer => {
+          sortAnswers(answer);
+        });
+    });
+  program.parse(process.argv);
 };
 
 main();
